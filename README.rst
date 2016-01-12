8e778f02219658ddf4d719071e42c78320aff7eb
Linus Törngren <linus@etnolit.se>
2016-01-12 10:45:37 +0100

    Basic structure of repository.

---
+++ README.rst
@@ -0,0 +1,47 @@
+Övningar: Datastrukturer och algoritmer
+=======================================
+
+Detta repository innehåller övningar som behandlar datastrukturer och
+algoritmer.
+
+
+Övningarna
+----------
+Övningarna finns i moduler i paketet exercises.
+Förväntad funktion finns i de docstrings som hör ihop med funktionerna.
+
+För att köra linter och enhetstester kan du använda följande kommandon.
+
+.. code-block::
+
+  python manage.py lint
+  python manage.py test
+
+För vissa övningar förväntas du själv skriva enhetstester. Detta görs med
+fördel i testfiler med beskrivande namn. (De måste ha formen test_*.py.)
+
+Innan du börjar
+---------------
+Skapa den virtuella körmiljön med de paket som behövs.
+
+.. code-block::
+
+  pyvenv venv
+
+Aktivera den virtella körmiljön:
+
+.. code-block::
+
+  . venv/bin/activate
+
+Om du använder Windows:
+
+.. code-block::
+
+  venv/Script/activate.bat
+
+Installera de paket som behövs för uppgiften:
+
+.. code-block::
+
+  pip install -r requirements.txt
---
+++ manage.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+import click
+
+APP_FOLDER = 'exercises'
+
+@click.group()
+def cli():
+    pass
+
+@click.command()
+@click.option('--coverage', 'with_coverage', is_flag=True)
+@click.option('--no-html', is_flag=True)
+@click.option('--no-report', is_flag=True)
+def test(with_coverage, no_html, no_report):
+    if with_coverage:
+        # Initialize coverage.py.
+        import coverage
+        COV = coverage.coverage(branch=True, include='{}}/*'.format(APP_FOLDER))
+        COV.start()
+
+    # Run all unit tests found in tests folder.
+    click.echo('Running autodiscovered tests\n{}'.format('=' * 70))
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    results = unittest.TextTestRunner(verbosity = 2).run(tests)
+
+    if with_coverage:
+        # Sum up the results of the code coverage analysis.
+        COV.stop()
+        COV.save()
+
+        if not no_html:
+            # Generate HTML report and move to tmp directory.
+            import os
+            basedir = os.path.abspath(os.path.dirname(__file__))
+            covdir = os.path.join(basedir, 'tmp/coverage')
+            COV.html_report(directory = covdir)
+
+        if not no_report:
+            # Show the report and clean up.
+            click.echo('\nCoverage Summary\n{}'.format('=' * 70))
+            COV.report()
+            COV.erase()
+
+    if not results.wasSuccessful():
+        # Make sure to get a non-zero exit code when failing.
+        raise click.ClickException('Test suite failed.')
+
+
+@click.command()
+def lint():
+    from flake8 import main as flake8
+    import sys
+
+    click.echo('Running Linter\n{}'.format('=' * 70))
+    sys.argv = ['flake8', APP_FOLDER]
+    flake8.main()
+
+
+cli.add_command(test)
+cli.add_command(lint)
+
+if __name__ == "__main__":
+    cli()
