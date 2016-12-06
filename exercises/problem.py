6bfd884d12c35b4248b037587aa5b8a0325fcbdf
= <=>
2016-12-06 09:55:29 +0100

    Changed problem generation to a completely connected graph.

--- exercises/problem.py
+++ exercises/problem.py
@@ -36,45 +36,13 @@ def generate(x_size, y_size):
         g.add_vertex(name)
         logger.debug('Adding node {}'.format(name))
 
-    # Randomly add a few edges per node to reasonably close nodes.
-    # Make sure that we connect each node to at least two other nodes.
+    # Add edges to all other nodes.
     for name, (x, y) in coordinates.items():
-        possible_neighbours = list(coordinates.keys())
-        possible_neighbours.remove(name)
-        nb_names = random.sample(possible_neighbours, random.randint(2, 5))
-        for nb_name in nb_names:
-            nb_x, nb_y = coordinates[nb_name]
-            if not nb_name in g.neighbours(name):
+        nodes = list(coordinates.items())
+        for nb_name, (nb_x, nb_y) in nodes:
+            if not name == nb_name and not nb_name in g.neighbours(name):
                 cost = abs(x - nb_x) + abs(y - nb_y)  # Manhattan distance between nodes
                 g.add_edge(name, nb_name, cost)
                 g.add_edge(nb_name, name, cost)
                 logger.debug('Added edge from {} to {} with cost {}.'.format(name, nb_name, cost))
-
-    # Verify that all nodes are reachable
-    logger.debug('Exploring graph to see that all nodes can be reached.')
-    all_nodes = list(coordinates.keys())
-    reached_nodes = [all_nodes[0]]
-    unexplored_nodes = all_nodes[1:]
-
-    queue = [all_nodes[0]]
-    while len(queue) > 0:
-        node = queue.pop(0)
-        logger.debug('Visiting node {}.'.format(node))
-        for n in g.neighbours(node):
-            if not n in reached_nodes:
-                reached_nodes.append(n)
-                unexplored_nodes.remove(n)
-                queue.append(n)
-                logger.debug('First appearance of node {}.'.format(n))
-
-    if len(unexplored_nodes) > 0:
-        logger.debug('Connecting unreached nodes...')
-        nb_names = random.sample(reached_nodes, 2)
-        for nb_name in nb_names:
-            nb_x, nb_y = coordinates[nb_name]
-            cost = abs(x - nb_x) + abs(y - nb_y)  # Manhattan distance between nodes
-            g.add_edge(name, nb_name, cost)
-            g.add_edge(nb_name, name, cost)
-            logger.debug('Added edge from {} to {} with cost {}.'.format(name, nb_name, cost))
-
     return g
